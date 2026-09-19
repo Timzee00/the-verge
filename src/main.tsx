a1f50e8976c58b572d3b05bc0365e4091c521b97
Timileyin Ayomide Salawudeen <timzeeworld@gmail.com>
2026-09-19 20:38:42 +0100

    Register THE VERGE offline application shell

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './ui/App';
 import { ErrorBoundary } from './ui/ErrorBoundary';
 import './styles.css';
+import { registerTheVergePWA } from './pwa';
+void registerTheVergePWA();
 createRoot(document.getElementById('root')!).render(<React.StrictMode><ErrorBoundary><App/></ErrorBoundary></React.StrictMode>);
